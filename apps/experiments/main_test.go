4ee5e751a01111fc57162598f9467eadba7ad2f8
Sergey Berezin <sergey@sergeyberezin.com>
2022-07-11 19:43:12 -0700

    Connect the Experiment API to the main app

--- apps/experiments/main_test.go
+++ apps/experiments/main_test.go
@@ -15,15 +15,28 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/stockparfait/experiments"
 	"github.com/stockparfait/logging"
+	"github.com/stockparfait/stockparfait/plot"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestMain(t *testing.T) {
 	t.Parallel()
+	tmpdir, tmpdirErr := ioutil.TempDir("", "testdb")
+	defer os.RemoveAll(tmpdir)
+
+	Convey("Test setup succeeded", t, func() {
+		So(tmpdirErr, ShouldBeNil)
+	})
 
 	Convey("parseFlags", t, func() {
 		flags, err := parseFlags([]string{
@@ -32,5 +45,64 @@ func TestMain(t *testing.T) {
 		So(flags.DBDir, ShouldEqual, "path/to/cache")
 		So(flags.Config, ShouldEqual, "c.json")
 		So(flags.LogLevel, ShouldEqual, logging.Warning)
+	})
+
+	Convey("run a test experiment end to end", t, func() {
+		confJSON := `
+{
+  "groups": [{"name": "xy", "graphs": [{"name": "r1"}]}],
+  "experiments": [{"test": {"graph": "r1"}}]
+}`
+		confPath := filepath.Join(tmpdir, "config.json")
+
+		// Run in a function closure to ensure the written file is closed before
+		// reading it.
+		(func() {
+			f, err := os.OpenFile(confPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+			So(err, ShouldBeNil)
+			_, err = f.WriteString(confJSON)
+			So(err, ShouldBeNil)
+			defer f.Close()
+		})()
+
+		dataJs := filepath.Join(tmpdir, "data.js")
+		dataJSON := filepath.Join(tmpdir, "data.json")
+
+		flags, err := parseFlags([]string{
+			"-conf", confPath, "-js", dataJs, "-json", dataJSON})
+		So(err, ShouldBeNil)
+
+		ctx := context.Background()
+		ctx = logging.Use(ctx, logging.DefaultGoLogger(logging.Info))
+		canvas := plot.NewCanvas()
+		values := make(experiments.Values)
+		ctx = plot.Use(ctx, canvas)
+		ctx = experiments.UseValues(ctx, values)
+
+		So(run(ctx, flags), ShouldBeNil)
+
+		So(values, ShouldResemble, map[string]string{
+			"grade": "2",
+			"test":  "failed",
+		})
+
+		jsonFile, err := os.Open(dataJSON)
+		So(err, ShouldBeNil)
+		defer jsonFile.Close()
+
+		var JSONbuf bytes.Buffer
+		_, err = JSONbuf.ReadFrom(jsonFile)
+		So(err, ShouldBeNil)
+		So(JSONbuf.String(), ShouldContainSubstring, `{"Groups":[{"Kind":"KindXY","XLogScale":false,"Graphs":[{"Kind":"KindXY","Title":"","XLabel":"","YLogScale":false,"PlotsRight":[{"Kind":"KindXY","X":[1,2],"Y":[21.5,42],"YLabel":"values","Legend":"Unnamed","ChartType":"ChartLine"}],"PlotsLeft":null}],"MinX":1,"MaxX":2}]}`)
+
+		jsFile, err := os.Open(dataJs)
+		So(err, ShouldBeNil)
+		defer jsonFile.Close()
+
+		var jsBuf bytes.Buffer
+		_, err = jsBuf.ReadFrom(jsFile)
+		So(err, ShouldBeNil)
+		So(jsBuf.String(), ShouldContainSubstring, `var DATA = {"Groups":[{"Kind":"KindXY","XLogScale":false,"Graphs":[{"Kind":"KindXY","Title":"","XLabel":"","YLogScale":false,"PlotsRight":[{"Kind":"KindXY","X":[1,2],"Y":[21.5,42],"YLabel":"values","Legend":"Unnamed","ChartType":"ChartLine"}],"PlotsLeft":null}],"MinX":1,"MaxX":2}]}`)
+
 	})
 }
