ffed80d29c0769a42df7e4752bf55a157f697dda
Sergey Berezin <sergey@sergeyberezin.com>
2022-07-11 20:27:00 -0700

    Connect the Experiment API to the main app (#5)

--- apps/experiments/main.go
+++ apps/experiments/main.go
@@ -16,29 +16,40 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/stockparfait/errors"
+	"github.com/stockparfait/experiments"
+	"github.com/stockparfait/experiments/config"
 	"github.com/stockparfait/logging"
+	"github.com/stockparfait/stockparfait/plot"
 )
 
 type Flags struct {
-	DBDir    string // default: ~/.stockparfait/sharadar
-	Config   string // required
-	LogLevel logging.Level
+	DBDir        string // default: ~/.stockparfait/sharadar
+	Config       string // required
+	LogLevel     logging.Level
+	DataJsPath   string // write data.js to this path
+	DataJSONPath string // write data.json to this path
 }
 
 func parseFlags(args []string) (*Flags, error) {
 	var flags Flags
-	fs := flag.NewFlagSet("sharadar", flag.ExitOnError)
+	fs := flag.NewFlagSet("experiments", flag.ExitOnError)
 	fs.StringVar(&flags.DBDir, "cache",
 		filepath.Join(os.Getenv("HOME"), ".stockparfait", "sharadar"),
 		"database path")
 	fs.StringVar(&flags.Config, "conf", "", "configuration file (required)")
 	flags.LogLevel = logging.Info
 	fs.Var(&flags.LogLevel, "log-level", "Log level: debug, info, warning, error")
+	fs.StringVar(&flags.DataJsPath, "js", "", "file to write 'data.js' plots")
+	fs.StringVar(&flags.DataJSONPath, "json", "", "file to write 'data.json' plots")
 
 	err := fs.Parse(args)
 	if err != nil {
@@ -50,11 +61,134 @@ func parseFlags(args []string) (*Flags, error) {
 	return &flags, err
 }
 
-func run(ctx context.Context, flags *Flags) error {
-	logging.Infof(ctx, "nothing to do")
+func loadConfig(configPath string) (*config.Config, error) {
+	f, err := os.Open(configPath)
+	if err != nil {
+		return nil, errors.Annotate(err, "cannot open config file '%s'", configPath)
+	}
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+	var jv interface{}
+	if err := dec.Decode(&jv); err != nil && err != io.EOF {
+		return nil, errors.Annotate(err, "failed to decode JSON in %s", configPath)
+	}
+
+	var c config.Config
+	if err := c.InitMessage(jv); err != nil {
+		return nil, errors.Annotate(err, "cannot interpret config '%s'", configPath)
+	}
+	return &c, nil
+}
+
+func addGroups(ctx context.Context, groups []*config.Group) error {
+	for _, gc := range groups {
+		kind := plot.KindXY
+		if gc.Timeseries {
+			kind = plot.KindSeries
+		}
+		group := plot.NewGroup(kind, gc.Name).SetXLogScale(gc.XLogScale)
+		if err := plot.AddGroup(ctx, group); err != nil {
+			return errors.Annotate(err, "cannot add group '%s'", gc.Name)
+		}
+		for _, gpc := range gc.Graphs {
+			graph, err := plot.EnsureGraph(ctx, kind, gpc.Name, gc.Name)
+			if err != nil {
+				return errors.Annotate(err, "cannot ensure graph '%s' in group '%s'",
+					gpc.Name, gc.Name)
+			}
+			graph.SetTitle(gpc.Title).
+				SetXLabel(gpc.XLabel).
+				SetYLogScale(gpc.YLogScale)
+		}
+	}
 	return nil
 }
 
+func runExperiment(ctx context.Context, ec config.ExperimentConfig) error {
+	var e experiments.Experiment
+	switch ec.Name() {
+	case "test":
+		e = &experiments.TestExperiment{}
+	default:
+		return errors.Reason("unsupported experiment '%s'", ec.Name())
+	}
+	if err := e.Run(ctx, ec); err != nil {
+		return errors.Annotate(err, "failed experiment '%s'", ec.Name())
+	}
+	return nil
+}
+
+func printValues(ctx context.Context) error {
+	keys := []string{}
+	values := experiments.GetValues(ctx)
+	if values == nil {
+		return errors.Reason("no values in context")
+	}
+	for k := range values {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	for _, k := range keys {
+		fmt.Printf("%s: %s\n", k, values[k])
+	}
+	return nil
+}
+
+func writePlots(ctx context.Context, flags *Flags) error {
+	if flags.DataJsPath != "" {
+		f, err := os.OpenFile(flags.DataJsPath,
+			os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return errors.Annotate(err, "cannot open file for writing :'%s'",
+				flags.DataJsPath)
+		}
+		defer f.Close()
+
+		if err := plot.WriteJS(ctx, f); err != nil {
+			return errors.Annotate(err, "failed to write '%s'", flags.DataJsPath)
+		}
+	}
+	if flags.DataJSONPath != "" {
+		f, err := os.OpenFile(flags.DataJSONPath,
+			os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return errors.Annotate(err, "cannot open file for writing :'%s'",
+				flags.DataJSONPath)
+		}
+		defer f.Close()
+
+		if err := plot.WriteJSON(ctx, f); err != nil {
+			return errors.Annotate(err, "failed to write '%s'", flags.DataJSONPath)
+		}
+	}
+	return nil
+}
+
+func run(ctx context.Context, flags *Flags) error {
+	cfg, err := loadConfig(flags.Config)
+	if err != nil {
+		return errors.Annotate(err, "failed to load config")
+	}
+	if err := addGroups(ctx, cfg.Groups); err != nil {
+		return errors.Annotate(err, "failed to add groups")
+	}
+	for _, e := range cfg.Experiments {
+		if err := runExperiment(ctx, e.Config); err != nil {
+			return errors.Annotate(err, "failed to run experiment '%s'",
+				e.Config.Name())
+		}
+	}
+	if err := printValues(ctx); err != nil {
+		return errors.Annotate(err, "failed to print values")
+	}
+	if err := writePlots(ctx, flags); err != nil {
+		return errors.Annotate(err, "failed to write plots")
+	}
+	return nil
+}
+
+// main should remain minimal, as it is not unit-tested due to os.Exit.
 func main() {
 	ctx := context.Background()
 	flags, err := parseFlags(os.Args[1:])
@@ -64,6 +198,10 @@ func main() {
 		os.Exit(1)
 	}
 	ctx = logging.Use(ctx, logging.DefaultGoLogger(flags.LogLevel))
+	canvas := plot.NewCanvas()
+	values := make(experiments.Values)
+	ctx = plot.Use(ctx, canvas)
+	ctx = experiments.UseValues(ctx, values)
 
 	if err := run(ctx, flags); err != nil {
 		logging.Errorf(ctx, err.Error())
