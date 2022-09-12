1cfda8d7e6516765a74f5629c40e872cd94c4fa6
Sergey Berezin <sergey@sergeyberezin.com>
2022-09-11 18:07:40 -0700

    README: mean, MAD and sigma for normal distribution (#49)

---
+++ powerdist/assets/assets_test.go
@@ -0,0 +1,40 @@
+// Copyright 2022 Stock Parfait
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package assets
+
+import (
+	"testing"
+
+	"github.com/stockparfait/experiments/config"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestConfigs(t *testing.T) {
+	t.Parallel()
+
+	configs := []string{
+		"normal-N-20M-mean-mad-sigma.json",
+		"normal-N-250-mean-mad-sigma.json",
+		"normal-N-5K-mean-mad-sigma.json",
+	}
+
+	Convey("Configs parse successfully", t, func() {
+		for _, conf := range configs {
+			_, err := config.Load(conf)
+			So(err, ShouldBeNil)
+		}
+	})
+}
