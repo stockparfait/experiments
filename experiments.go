b59d6076be34cddc935dbc2c1c2e5e47a6da72c6
Sergey Berezin <sergey@sergeyberezin.com>
2022-10-29 19:16:53 -0700

    Self-review

--- experiments.go
+++ experiments.go
@@ -178,7 +178,7 @@ func PlotDistribution(ctx context.Context, dh stats.DistributionWithHistogram, c
 		return errors.Annotate(err, "failed to add value for '%s P(X < mean-10*sigma)'", legend)
 	}
 	if err := AddValue(ctx, legend+" P(X > mean+10*sigma)", fmt.Sprintf("%.4g", 1.0-dh.CDF(dh.Mean()+10*math.Sqrt(dh.Variance())))); err != nil {
-		return errors.Annotate(err, "failed to add value for '%s P(X < mean-10*sigma)'", legend)
+		return errors.Annotate(err, "failed to add value for '%s P(X > mean+10*sigma)'", legend)
 	}
 	return nil
 }
